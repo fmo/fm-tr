98d02afb186a207828191b339144e3876e215873
Mustafa Özyurt <mustafa.ozyurt@gmail.com>
2021-08-15 10:49:01 +0200

    Controller definitions in route file

---
+++ src/Controller/Country/AddController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller\Country;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AddController extends AbstractController
+{
+
+}
---
+++ src/Controller/Country/ListController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Controller\Country;
+
+class ListController
+{
+
+}
