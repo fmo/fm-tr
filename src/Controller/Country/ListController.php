30c45093c263553db607de0a94034fb77f6b9ea1
Mustafa Özyurt <mustafa.ozyurt@gmail.com>
2021-08-17 09:47:45 +0200

    List Controller definitions

--- src/Controller/Country/ListController.php
+++ src/Controller/Country/ListController.php
@@ -2,7 +2,28 @@
 
 namespace App\Controller\Country;
 
-class ListController
-{
+use App\Exception\NoCountryException;
+use App\Repository\CountryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class ListController extends AbstractController
+{
+    public function __construct(
+        private CountryRepository $countryRepository
+    )
+    {
+    }
+
+    public function __invoke()
+    {
+        $countries = $this->countryRepository->findAll();
+
+        if (!$countries) {
+            throw new NoCountryException("No country found");
+        }
+
+        return $this->render('country/list.html.twig', [
+            'countries' => $countries
+        ]);
+    }
 }
