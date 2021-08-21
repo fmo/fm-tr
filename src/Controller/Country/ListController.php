3c18f1a4798c22829fa6671903c3ccb8afeeb313
Mustafa Özyurt <mustafa.ozyurt@gmail.com>
2021-08-21 13:44:47 +0200

    List Country

--- src/Controller/Country/ListController.php
+++ src/Controller/Country/ListController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Country;
 use App\Exception\NoCountryException;
 use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class ListController extends AbstractController
 {
@@ -14,7 +15,7 @@ class ListController extends AbstractController
     {
     }
 
-    public function __invoke()
+    public function __invoke(): Response
     {
         $countries = $this->countryRepository->findAll();
 
@@ -22,7 +23,7 @@ class ListController extends AbstractController
             throw new NoCountryException("No country found");
         }
 
-        return $this->render('country/list.html.twig', [
+        return $this->render('Country/list.html.twig', [
             'countries' => $countries
         ]);
     }
